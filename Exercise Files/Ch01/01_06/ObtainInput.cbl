4bfdc99eb5d0fc5345483ded7b99ed97ad0f2a8a
motalik <riccardone.dose@gmail.com>
2023-10-17 23:48:53 +0200

    esercizi

---
+++ Exercise Files/Ch01/01_06/ObtainInput.cbl
@@ -0,0 +1,13 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. "OBTAININPUT".
+       AUTHOR.     PEGGY FISHER.
+      *This is a comment in COBOL
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+     
+       PROCEDURE DIVISION. 
+           0100-START-HERE.
+               DISPLAY "Hello World!".
+       STOP RUN.
+       END PROGRAM OBTAININPUT.
