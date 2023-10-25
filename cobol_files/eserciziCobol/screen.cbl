1479ec68963d78dd6de25765388812bda5ea9705
motalik <riccardone.dose@gmail.com>
2023-10-26 00:00:21 +0200

    Agg

--- cobol_files/eserciziCobol/screen.cbl
+++ cobol_files/eserciziCobol/screen.cbl
@@ -1,123 +1,146 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. screen-cbl.
+
        ENVIRONMENT DIVISION.
-       CONFIGURATION SECTION.
-       SPECIAL-NAMES.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-       SELECT FileUtenti ASSIGN TO "DatiUtente.dat"
-           ORGANIZATION IS SEQUENTIAL
-           ACCESS IS SEQUENTIAL.
-       SELECT ReportUtente ASSIGN TO "ReportUtente.dat"
-           ORGANIZATION IS LINE SEQUENTIAL.
+          SELECT DatiUtenteFile ASSIGN TO "DatiUtente.dat"
+              ORGANIZATION IS SEQUENTIAL
+              ACCESS IS SEQUENTIAL.
+          SELECT ReportFile ASSIGN TO "Report.dat"
+              ORGANIZATION IS LINE SEQUENTIAL.
+
        DATA DIVISION.
        FILE SECTION.
-       FD FileUtenti.
-       01 DatiUtenti.
+       FD DatiUtenteFile.
+       01 DatiUtenteRecord.
           02 IDNum PIC X(4).
           02 COMPANY-NAME PIC X(30).
           02 FIRST-NAME PIC X(15).
           02 LAST-NAME PIC X(25).
           02 MAIL-ADDRESS PIC X(30).
 
-       FD FileUtente.  <<--- Define the FileUtente file description
-       01 YourFileRecord.
-          02 ...  <<-- Define the structure of records in FileUtente
+       FD ReportFile
+          BLOCK CONTAINS 0 RECORDS
+          RECORDING MODE F.
+          01 PrintLine PIC X(200).
+          01 filler PIC X VALUE SPACE.
+          01 PrintCustID PIC 9(20).
+          01 filler PIC X VALUE SPACE.
+          01 PrintFirstName PIC X(50).
+          01 filler PIC X VALUE SPACE.
+          01 PrintLastName PIC X(55).
+          01 filler PIC X VALUE SPACE.
+          01 PrintAzienda PIC X(50).
+          01 filler PIC X VALUE SPACE.
+          01 PrintEmail PIC X(50).
+       
 
        WORKING-STORAGE SECTION.
-       01 WS-DatiUtenti.
-          02 WS-Active-Spaces VALUE SPACES.
-          02 WS-Company-Name PIC X(30).
-          02 WS-Last-Name PIC X(25).
-          02 WS-First-Name PIC X(15).
-          02 WS-Mail-Address PIC X(30).
-          02 WS-IDNum PIC X(4).
+       01 WS-IDNum PIC X(4).
+       01 WS-Company-Name PIC X(30).
+       01 WS-First-Name PIC X(15).
+       01 WS-Last-Name PIC X(25).
+       01 WS-Mail-Address PIC X(30).
 
        01 PageHeading.
-          02 filler PIC X(13) VALUE "Customer List".
+          02 filler PIC X(40) VALUE "         ---Customer List---".
        01 PageFooting.
           02 Filler PIC X(15) VALUE SPACES.
           02 filler PIC X(7) VALUE "Page : ".
           02 PrnPageNum PIC Z9.
 
-       01 Heads PIC X(36) VALUE "IDNum     FirstName     LastName".
        01 InformazioniUtente.
           02 filler PIC X VALUE SPACE.
-          02 PrnCustID PIC 9(5).
-          02 filler PIC X(4) VALUE SPACE.
-          02 PrnFirstName PIC X(15).
-          02 filler PIC XX VALUE SPACE.
-          02 PrnLastName PIC X(15).
+          02 PrnCustID PIC 9(4).
+          02 filler PIC X VALUE SPACE.
+          02 PrnFirstName PIC X(16).
+          02 filler PIC X VALUE SPACE.
+          02 PrnLastName PIC X(55).
+          02 filler PIC X VALUE SPACE.
+          02 PrnAzienda PIC X(30).
+          02 filler PIC X VALUE SPACE.
+          02 PrnEmail PIC X(50).
 
        01 ReportFooting PIC X(13) VALUE "End of report".
        01 LineCount PIC 99 VALUE ZERO.
-          88 NewPageRequired VALUE 40 THRU 99.
+       01 NewPageRequired PIC 99 VALUE 0.
        01 PageCount PIC 99 VALUE ZERO.
-
-       01 WSEOF PIC X VALUE 'N'.  <<-- Define WSEOF
-
+       01 WSEOF PIC 9 VALUE 0.
        SCREEN SECTION.
        01 CLEAR-SCREEN.
-          05 BLANK SCREEN BACKGROUND-COLOR 15 FOREGROUND-COLOR 0.
-       01 INPUT-SCREEN AUTO.
+          05 BLANK SCREEN.
+       01 INPUT-SCREEN.
           02 LINE 6 COLUMN 20 VALUE IS 'ID: '.
-          02 USING WS-IDNum REVERSE-VIDEO PIC X(4).
+          02 COLUMN 24 PIC X(4) USING WS-IDNum.
           02 LINE 7 COLUMN 20 VALUE IS 'Azienda: '.
-          02 USING WS-Company-Name REVERSE-VIDEO PIC X(30).
+          02 COLUMN 28 PIC X(30) USING WS-Company-Name.
           02 LINE 8 COLUMN 20 VALUE IS 'Nome: '.
-          02 USING WS-First-Name REVERSE-VIDEO PIC X(15).
+          02 COLUMN 25 PIC X(15) USING WS-First-Name.
           02 LINE 9 COLUMN 20 VALUE IS 'Cognome: '.
-          02 USING WS-Last-Name REVERSE-VIDEO PIC X(25).
+          02 COLUMN 30 PIC X(25) USING WS-Last-Name.
           02 LINE 10 COLUMN 20 VALUE IS 'Email: '.
-          02 USING WS-Mail-Address REVERSE-VIDEO PIC X(30).
+          02 COLUMN 28 PIC X(30) USING WS-Mail-Address.
 
        PROCEDURE DIVISION.
        100-MAIN-MODULE.
-          OPEN OUTPUT FileUtenti
-          MOVE SPACES TO WS-IDNum
-                      WS-Company-Name
-                      WS-First-Name
-                      WS-Last-Name
-                      WS-Mail-Address
           DISPLAY CLEAR-SCREEN
           DISPLAY INPUT-SCREEN
           ACCEPT INPUT-SCREEN
+          OPEN OUTPUT DatiUtenteFile
           MOVE WS-IDNum TO IDNum
           MOVE WS-Company-Name TO COMPANY-NAME
           MOVE WS-First-Name TO FIRST-NAME
           MOVE WS-Last-Name TO LAST-NAME
           MOVE WS-Mail-Address TO MAIL-ADDRESS
-          WRITE DatiUtenti
+          WRITE DatiUtenteRecord
           END-WRITE
-          CLOSE FileUtenti
+          CLOSE DatiUtenteFile
 
-          OPEN INPUT FileUtente
-          OPEN OUTPUT ReportUtente
+          OPEN INPUT DatiUtenteFile
+          OPEN OUTPUT ReportFile
           PERFORM PrintPageHeading
-          READ FileUtente AT END SET WSEOF TO 'Y'  <<-- Set WSEOF to 'Y' when end of file is reached
+          READ DatiUtenteFile AT END SET WSEOF TO 1
           END-READ
-          PERFORM PrintReportBody UNTIL WSEOF = 'Y'  <<-- Check if WSEOF is 'Y'
+          PERFORM UNTIL WSEOF = 1
+              PERFORM PrintReportBody
+          END-PERFORM
           WRITE PrintLine FROM ReportFooting AFTER ADVANCING 5 LINES
-          CLOSE FileUtente ReportUtente
+          CLOSE DatiUtenteFile, ReportFile.
           STOP RUN.
-
        PrintPageHeading.
           WRITE PrintLine FROM PageHeading AFTER ADVANCING Page
-          WRITE PrintLine FROM Heads AFTER ADVANCING 5 LINES
           MOVE 3 TO LineCount
           ADD 1 TO PageCount.
 
        PrintReportBody.
-          IF NewPageRequired
-              MOVE PageCount TO PrnPageNum
+          IF LineCount IS GREATER THAN 40
+              MOVE 1 TO PrnPageNum
+              MOVE 0 TO LineCount
               WRITE PrintLine FROM PageFooting AFTER ADVANCING 5 LINES
               PERFORM PrintPageHeading
-          END-IF
+          END-IF.
           MOVE IDNum TO PrnCustID
           MOVE FIRST-NAME TO PrnFirstName
           MOVE LAST-NAME TO PrnLastName
-          WRITE PrintLine FROM InformazioniUtente AFTER ADVANCING 1 LINE
+          MOVE COMPANY-NAME TO PrnAzienda
+          MOVE MAIL-ADDRESS TO PrnEmail
+          MOVE "IDNum: " TO PrintLine
+          MOVE PrnCustID TO PrintCustID(10:20)
+          write PrintCustID after advancing 1 line  
+          MOVE "Azienda: " TO PrintLine
+          MOVE PrnAzienda TO PrintAzienda(10:50)
+          write PrintAzienda after advancing 1 line
+          MOVE "Nome: " TO PrintLine
+          MOVE PrnFirstName TO PrintFirstName(10:50)
+          write PrintFirstName after advancing 1 line
+          MOVE "Cognome: " TO PrintLine
+          MOVE PrnLastName TO PrintLastName(10:50)
+          write PrintLastName after advancing 1 line
+          MOVE "Email: " TO PrintLine
+          MOVE PrnEmail TO PrintEmail(10:50)
+          write PrintEmail after advancing 1 line
           ADD 1 TO LineCount
-          READ FileUtente AT END SET WSEOF TO 'Y'  <<-- Set WSEOF to 'Y' when end of file is reached
+          READ DatiUtenteFile AT END SET WSEOF TO 1
           END-READ.
        STOP RUN.
